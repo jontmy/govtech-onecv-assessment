330e8dd57b8f4168e83c49646d91c6e88dbdaadd
Jonathan Tay <jonathantaymy@gmail.com>
2023-03-06 15:43:13 +0800

    feat: allow server to listen at port specified in .env

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 )
 
 // POST /api/register
@@ -42,17 +44,36 @@ func retrieveForNotifications(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
 }
 
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("error loading .env: %s\n", err)
+	}
+	return os.Getenv(key)
+}
+
 func main() {
+	// Register the API handlers.
 	http.HandleFunc("/api/register", register)
 	http.HandleFunc("/api/commonstudents", commonStudents)
 	http.HandleFunc("/api/suspend", suspend)
 	http.HandleFunc("/api/retrievefornotifications", retrieveForNotifications)
-	port := ":3333"
-	err := http.ListenAndServe(port, nil)
+
+	// Start the server at the port specified in .env.
+	port := getEnvVariable("PORT")
+	var addr string
+	if port == "" {
+		addr = ":3000"
+	} else {
+		addr = ":" + port
+	}
+	err := http.ListenAndServe(addr, nil)
 	fmt.Printf("info: server listening on port %s\n", port)
+
+	// Handle server initialization errors.
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("error: server closed.\n")
 	} else if err != nil {
-		fmt.Printf("error: %s\n", err)
+		fmt.Printf("error while starting server: %s\n", err)
 	}
 }
