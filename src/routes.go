df6477e7ec9215ebff776690b4105d45152ddf44
Jonathan Tay <jonathantaymy@gmail.com>
2023-03-07 15:47:14 +0800

    feat: implement POST /api/register

--- src/routes.go
+++ src/routes.go
@@ -1,18 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
+	"database/sql"
+	"encoding/json"
 	"net/http"
-	"os"
 )
+
+var db *sql.DB
+
+type Registration struct {
+	Teacher  string   `json:"teacher"`
+	Students []string `json:"students"`
+}
 
 // POST /api/register
 func register(res http.ResponseWriter, req *http.Request) {
+	// Check that the request method is POST.
 	if req.Method != http.MethodPost {
 		res.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+	// Check that the request body is JSON.
+	if req.Header.Get("Content-Type") != "application/json" {
+		res.WriteHeader(http.StatusUnsupportedMediaType)
+		return
+	}
+	// Parse the request body.
+	var registration Registration
+	err := json.NewDecoder(req.Body).Decode(&registration)
+	if err != nil {
+		res.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+	// Insert the teacher into the database if they don't already exist.
+	_, err = db.Exec("INSERT IGNORE INTO teachers (email) VALUES (?)", registration.Teacher)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Insert the students into the database if they don't already exist.
+	for _, student := range registration.Students {
+		_, err = db.Exec("INSERT IGNORE INTO students (email) VALUES (?)", student)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+	// Send the response.
 	res.WriteHeader(http.StatusNoContent)
 }
 
@@ -41,12 +75,4 @@ func retrieveForNotifications(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	res.WriteHeader(http.StatusOK)
-}
-
-func getEnvVariable(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf("error loading .env: %s\n", err)
-	}
-	return os.Getenv(key)
 }
