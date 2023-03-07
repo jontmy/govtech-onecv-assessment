13dce785423688084a11c5484b675789f75f2153
Jonathan Tay <jonathantaymy@gmail.com>
2023-03-07 17:05:39 +0800

    feat(db): remove teacher and student ids

--- mysql/db_ddl.sql
+++ mysql/db_ddl.sql
@@ -1,24 +1,18 @@
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS teachers, students, teaches;
+DROP TABLE IF EXISTS teachers, students, class;
 SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE teachers (
-    id    INT          NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY (id)
+    teacher_email VARCHAR(255) PRIMARY KEY
 );
 
 CREATE TABLE students (
-    id           INT          NOT NULL AUTO_INCREMENT,
-    email        VARCHAR(255) NOT NULL UNIQUE,
-    is_suspended BOOLEAN      NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (id)
+    student_email VARCHAR(255) PRIMARY KEY,
+    is_suspended  BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE teaches (
-    teacher_id INT NOT NULL,
-    student_id INT NOT NULL,
-    PRIMARY KEY (teacher_id, student_id),
-    FOREIGN KEY (teacher_id) REFERENCES teachers (id),
-    FOREIGN KEY (student_id) REFERENCES students (id)
+CREATE TABLE class (
+    teacher_email VARCHAR(255) NOT NULL REFERENCES teachers (teacher_email),
+    student_email VARCHAR(255) NOT NULL REFERENCES students (student_email),
+    PRIMARY KEY (teacher_email, student_email)
 );
