4ab6f153c717d4bf7984d3ad71efce448e99056a
Jonathan Tay <jonathantaymy@gmail.com>
2023-03-07 15:44:35 +0800

    feat: add database connection

--- src/main.go
+++ src/main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	// Register the API handlers.
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("error loading .env: %s\n", err)
+	}
+	return os.Getenv(key)
+}
+
+func registerAPIHandlers() {
 	http.HandleFunc("/api/register", register)
 	http.HandleFunc("/api/commonstudents", commonStudents)
 	http.HandleFunc("/api/suspend", suspend)
 	http.HandleFunc("/api/retrievefornotifications", retrieveForNotifications)
+}
 
+func connectDatabase() {
+	cfg := mysql.Config{
+		User:   getEnvVariable("DATABASE_USER"),
+		Passwd: getEnvVariable("DATABASE_PASSWORD"),
+		Net:    "tcp",
+		Addr:   getEnvVariable("DATABASE_URL"),
+		DBName: getEnvVariable("DATABASE_NAME"),
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Printf("info: connected to database '%s'.\n", cfg.DBName)
+}
+
+func startServer() {
 	// Start the server at the port specified in .env.
 	port := getEnvVariable("PORT")
 	var addr string
@@ -22,12 +57,18 @@ func main() {
 		addr = ":" + port
 	}
 	err := http.ListenAndServe(addr, nil)
-	fmt.Printf("info: server listening on port %s\n", port)
+	fmt.Printf("info: server listening on port %s.\n", port)
 
 	// Handle server initialization errors.
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("error: server closed.\n")
 	} else if err != nil {
-		fmt.Printf("error while starting server: %s\n", err)
+		fmt.Printf("error while starting server: %s.\n", err)
 	}
+}
+
+func main() {
+	registerAPIHandlers()
+	connectDatabase()
+	startServer()
 }
